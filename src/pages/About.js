ba8e0f9e80b45b73ccd7604cb6658128cde447b3
Julia Dearden <julia.anne4@gmail.com>
2023-12-13 13:21:11 -0500

    slight style tweaks

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,8 @@ function Home() {
         <section className="about">
             <img src={JDPortrait} className="about-image" loading="lazy" alt="Julia Dearden Portrait"/>
             <div className="about-text">
-                <h2 className="about-title">Julia Dearden</h2>
+                <div className="label">Hey there!</div>
+                <h2 className="about-title">I'm Julia Dearden</h2>
                 <h2 className="about-subtitle">frontend engineer & product designer</h2>
                 <p className="about-paragraph">As a result of working in various technical, creative, and strategic positions, I am a cross-functional problem solver who is passionate about front-end engineering and product design.<br/><br/>My drive is to create empathetic, human experiences within technology. I have a strong, technical background that is complemented with a foundation of design-thinking skills. I am obsessed with asking <b className="blue">“Why?”</b> and keeping humans at the center of dev and design decisions.</p>
                 <h4 className="about-personal">Other things I love: music, pilates, being outdoors, and painting!</h4>
