12b0f03b4dee3a906355508def904a8a25bb78d1
Julia Dearden <julia.anne4@gmail.com>
2024-04-05 17:32:18 -0400

    style clean up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -33,7 +33,7 @@ function Home() {
             <div className="about">
                 <div className="about-text">
                     <p className="about-paragraph">I am a passionate problem solver with design, development, and research experience. My love for product design, combined with my ability to understand technical concepts, allows me to approach problems uniquely and balance design with development trade-offs. I am obsessed with asking "Why?" and my design methodology revolves around keeping humans at the center of solutions.
-                    <br/><br/>This site is a suuuper simple overview of my past development projects. To see my resume, my experience more in-depth, and my product design work, visit my <a href="https://jdearden.webflow.io/" target="_blank" rel="noopener noreferrer" className="portfolio-link">design portfolio</a>.
+                    <br/><br/>This site is a suuuper simple overview of my past development projects. To see my experience more in-depth, and my product design work, visit my <a href="https://jdearden.webflow.io/" target="_blank" rel="noopener noreferrer" className="portfolio-link">design portfolio</a>.
                     </p>
                     <div className="link-container">
                         <a href="https://github.com/jdearden4/" target="_blank" rel="noopener noreferrer"><img className="icon" src={github} alt="email"/></a>
@@ -41,7 +41,9 @@ function Home() {
                         <a href="https://www.linkedin.com/in/julia-dearden/" target="_blank" rel="noopener noreferrer" className="portfolio-link"><img className="icon" src={linkedin} alt="linkedin"/></a>
                     </div>
                 </div>
-                <img src={JDPortrait} className="about-image" loading="lazy" alt="Julia Dearden Portrait"/>
+                <div className="about-image-background">
+                    <img src={JDPortrait} className="about-image" loading="lazy" alt="Julia Dearden Portrait"/>
+                </div>
             </div>
         </section>
     )
